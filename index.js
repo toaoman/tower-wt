f4f76fbea9ccbef78aff0fee34950d77c420f502
çetin turan <cturan@users.noreply.github.com>
2019-02-04 02:18:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 ﻿var app = require("express")(); 
 var http = require("http").Server(app); 
 var io = require("socket.io")(http); 
-var Usercounter = 0; 
-var elma;
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
